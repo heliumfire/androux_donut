d2dc30eb834ab020e9f39f4ec2619e9254e14620
heliumfire <regnarts299792458@gmail.com>
2015-05-15 13:09:19 +0530

    added androux code to external

---
+++ external/androux /mpfr/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  build.sh
+#  
+#
+#  Created by Anushruth on 17/12/2014.
+#
+
+make distclean
+./configure \
+--prefix=${PREFIX} \
+    --host=arm-linux-androideabi \
+    --target=arm-linux-androideabi \
+    --with-gmp=${PREFIX} \
+    CC=${CROSS_COMPILE}gcc \
+    CXX=${CROSS_COMPILE}g++ \
+    CFLAGS="-g -O2 -I${SYSROOT}/usr/include/ --sysroot=${SYSROOT}" \
+    LDFLAGS="-L${SYSROOT}/usr/lib/" \
+    CPP=${CROSS_COMPILE}cpp \
+    CPPFLAGS="-I${SYSROOT}/usr/include/" \
+    AR=${CROSS_COMPILE}ar \
+    NM=${CROSS_COMPILE}nm
+make clean
+make -j$THREADS
+make install
+make clean
